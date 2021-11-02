47be1f11c6815f5d0dad09eb6973e76aed097826
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-02 15:15:23 +0100

    inizio creazione pseudopassaggi

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('hey there!');
+/*
+Consegna L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (o simili, l’importante è dare all’utente il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).
+*/
+
+// user sceglie il livello del gioco cliccando sul bottone relativo
+// quindi in base al livello la griglia avrà 100 81 o 49 celle.
+
+//cicliamo la creazione di celle in base alla scelta
+
