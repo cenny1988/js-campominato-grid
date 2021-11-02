08c837929f1b09e65e6b3fd4c92b9c1edcaf49a4
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-02 16:18:53 +0100

    create funzioni per griglia da 91 e 49 elementi

--- js/main.js
+++ js/main.js
@@ -18,20 +18,39 @@ level1.addEventListener('click',
     level1Grid
 );
 
-// level2.addEventListener('click',
-//     //funzione crea 81 celle
-// );
+level2.addEventListener('click',
+    //funzione crea 81 celle
+    level2Grid
+);
 
-// level3.addEventListener('click',
-//     //funzione crea 49 celle
-// );
+level3.addEventListener('click',
+    //funzione crea 49 celle
+    level3Grid
+);
 
 
 
 // creazione for 100 celle con numerazione interna
 function level1Grid(){
-    for (i=0; i<100; i++) {
-        let square = createSquareWith2Class('square','ten')
+    let numCell = 100;
+    for (i=0; i<numCell; i++) {
+        let square = createSquareWith2Class('square','ten')//richiama funz per creare div con 2 classi da passare
+
+        let squareSpan = document.createElement('span');//crea span con numero cella
+        squareSpan.append(i+1);
+        
+        square.appendChild(squareSpan);//appendi span nella cella
+        
+        grid.appendChild(square);//appendi cella nella griglia
+    
+    }
+}
+
+// creazione for 81 celle con numerazione interna
+function level2Grid(){
+    let numCell = 81;
+    for (i=0; i<numCell; i++) {
+        let square = createSquareWith2Class('square','nine')
 
         let squareSpan = document.createElement('span');
         squareSpan.append(i+1);
@@ -43,6 +62,23 @@ function level1Grid(){
     }
 }
 
+// creazione for 49 celle con numerazione interna
+function level3Grid(){
+    let numCell = 49;
+    for (i=0; i<numCell; i++) {
+        let square = createSquareWith2Class('square','seven')
+
+        let squareSpan = document.createElement('span');
+        squareSpan.append(i+1);
+        
+        square.appendChild(squareSpan);
+        
+        grid.appendChild(square);
+    
+    }
+}
+
+// crea elemento div con 2 classi da passare 
 function createSquareWith2Class(class1,class2){
     let newSquare = document.createElement('div');
     newSquare.classList.add(class1,class2);
